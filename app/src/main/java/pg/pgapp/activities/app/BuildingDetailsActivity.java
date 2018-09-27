14a9c49b719f96f9339d46a49c67fb344b213399
Mateusz Szypulski <26szypulski2gmail.com>
2018-09-27 20:32:14 +0200

    Wstepny pomysl

--- app/src/main/java/pg/pgapp/activities/app/BuildingDetailsActivity.java
+++ app/src/main/java/pg/pgapp/activities/app/BuildingDetailsActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
@@ -44,9 +45,11 @@ public class BuildingDetailsActivity extends AppCompatActivity {
 					.append(facultyName)
 					.append("\n");
 		}
-		buildingFacultyTextView.setText(facultiesNames.toString());
-		buildingDescription.setText(building.getDescription());
-
+		//buildingFacultyTextView.setText(facultiesNames.toString());
+		buildingFacultyTextView.setText("Wydział elektroniki, telekomunikacji i informatyki (ETI)");
+		//buildingDescription.setText(building.getDescription());
+		buildingDescription.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum" +
+				"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\"");
 		byte[] decodedString = Base64.decode(building.getPicture(), Base64.DEFAULT);
 		Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 		imageView.setImageBitmap(decodedByte);
--- app/src/main/java/pg/pgapp/activities/app/FacultyDetailsActivity.java
+++ app/src/main/java/pg/pgapp/activities/app/FacultyDetailsActivity.java
@@ -3,11 +3,15 @@ package pg.pgapp.activities.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -30,11 +34,18 @@ public class FacultyDetailsActivity extends AppCompatActivity implements Adapter
 
 	private void initializeDetailView() {
 		TextView facultyNameTextView = findViewById(R.id.facultyNameTextView);
+		TextView readMoreTextView = findViewById(R.id.readMoreTextView);
+		readMoreTextView.setClickable(true);
+		readMoreTextView.setMovementMethod(LinkMovementMethod.getInstance());
+		String text = "<a href='https://eti.pg.edu.pl/katedra-inteligentnych-systemow-interaktywnych/o-katedrze'> Czytaj więcej... </a>";
+		readMoreTextView.setText(Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT));
+
 		Intent intent = getIntent();
 		Long tag = intent.getLongExtra("TAG", 2);
 
 		Faculty faculty = new DatabaseConnector().getFacultyModel(tag);
 		facultyNameTextView.setText(faculty.getName());
+
 
 		ArrayList<String> departmentsForSpinner = new ArrayList<>(faculty.getDepartmentsNames());
 		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, departmentsForSpinner);
@@ -44,9 +55,10 @@ public class FacultyDetailsActivity extends AppCompatActivity implements Adapter
 
 	@Override
 	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-		TextView selectedTV = findViewById(R.id.selectedTextView);
-		selectedTV.setText(spinner.getItemAtPosition(position).toString());
+		TextView departmentDescriptionTextView = findViewById(R.id.departmentDescription);
+		//TextView selectedTV = findViewById(R.id.selectedTextView);
+		//selectedTV.setText(spinner.getItemAtPosition(position).toString());
+		departmentDescriptionTextView.setText("Katedra Inteligentnych Systemów Interaktywnych jest jednym z pięciu filarów kierunku Informatyka na Wydziale Elektroniki, Telekomunikacji i Informatyki i działa w obecnie najszybciej rozwijających się obszarach technologii informacyjnych, stanowiących zręby rodzącego się społeczeństwa informacyjnego XXI wieku: interaktywnych i wielopostaciowych dokumentów cyfrowych, wirtualnych zespołów roboczych, inteligentnego gromadzenia i wyszukiwania informacji, widzenia komputerowego oraz wizualizacji informacji i zjawisk. ");
 	}
 
 	@Override
